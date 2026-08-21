2cac14b914d515c7162177a18886ff90c46681d6
hadangquoctoan <toanhadangquoc@gmail.com>
2026-08-21 07:55:53 +0700

    update lesson

--- src/01_Basics/Conditional.java
+++ src/01_Basics/Conditional.java
@@ -5,7 +5,17 @@ public class Conditional {
         if (isRaining) {
             System.out.println("Bring an umbrella !");
             // block of code to be executed if the condition is true
+            int x = 20;
+            int y = 18;
+            if (x > y) {
+                System.out.println("x is greater than y");
+            }
+            boolean isLightOn = true;
+
+            if (isLightOn) {
+                System.out.println("The light is on.");
+            }
         }
     }
-    
+
 }
